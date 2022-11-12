c025623186c94e02f63df089bf22b9301f9d11c6
Elifele Nathan <117702636+ElifeleNathan@users.noreply.github.com>
2022-11-12 22:38:20 +0800

    Add files via upload

---
+++ Menampilkan Stambuk dan Nama/src/MenampilkanStambukDanNama.java
@@ -0,0 +1,11 @@
+public class MenampilkanStambukDanNama {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("2263001");
+        System.out.println("I Ketut Natanael Elifele Sarjana");
+    }
+    
+}
