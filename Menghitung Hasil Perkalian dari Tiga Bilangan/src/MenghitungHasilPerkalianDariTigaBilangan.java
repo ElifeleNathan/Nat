027f703de9156499b24da832fbcaf76391d6c7b9
Elifele Nathan <117702636+ElifeleNathan@users.noreply.github.com>
2022-11-12 22:41:39 +0800

    Project

---
+++ Menghitung Hasil Perkalian dari Tiga Bilangan/src/MenghitungHasilPerkalianDariTigaBilangan.java
@@ -0,0 +1,14 @@
+public class MenghitungHasilPerkalianDariTigaBilangan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int nilaia=10;
+        int nilaib=20;
+        int nilaic=25;
+        int kali=nilaia*nilaib*nilaic;
+        System.out.println("hasil kali="+kali);
+    }
+    
+}
