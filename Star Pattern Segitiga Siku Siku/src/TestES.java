b56181db232806b604325fff76cc48b7e4b4ce19
Elifele Nathan <117702636+ElifeleNathan@users.noreply.github.com>
2022-11-15 17:36:20 +0800

    Project Star Iteraction in Java

---
+++ Star Pattern Segitiga Siku Siku/src/TestES.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author MSI_2022
+ */
+public class TestES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      //menambahkan variabel i dan j
+      int i, j;
+      //untuk variabel i di set dimana variabel i adalah 0 kemudian i lebih kecil dari 3 kemudian i ditambah 1
+      for(i=0; i<3; i++)
+      {
+          //untuk variabel j di set dimana variabel j adalah 0 kemudian variabel j di tambah 1
+         for(j=0; j<=i; j++)
+         {
+             //cetak bintang yang di hasilkan dari rumus diatas
+            System.out.print("* ");
+         }
+         //cetak ini untuk menyusun bintang agar lebih rapih (tidak sebaris)
+         System.out.print("\n");
+      }
+    }
+    
+}
