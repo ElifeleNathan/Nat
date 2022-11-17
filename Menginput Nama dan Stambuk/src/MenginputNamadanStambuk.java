9aca6308ad10d0725084bd0f998b8066f83aba9e
Elifele Nathan <117702636+ElifeleNathan@users.noreply.github.com>
2022-11-17 08:37:33 +0800

    Update and rename Pelajaran.java to MenginputNamadanStambuk.java

--- Menginput Nama dan Stambuk/src/MenginputNamadanStambuk.java
+++ Menginput Nama dan Stambuk/src/MenginputNamadanStambuk.java
@@ -5,14 +5,18 @@ public class Pelajaran {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+       //inisialisasi
        int stambuk;
        String nama;
-       
+       //set scanner
        Scanner a = new Scanner(System.in);
+       //cetak perintah
        System.out.print("Masukkan Stambuk Anda = ");
        System.out.println("Masukkan Nama Anda = ");
+       //set inputan
        stambuk = a.nextInt();
        nama = a.nextLine();
+       //cetak inputan user
        System.out.println("Stambuk Anda = " +stambuk);
        System.out.println("Nama Anda = " + nama);
        
