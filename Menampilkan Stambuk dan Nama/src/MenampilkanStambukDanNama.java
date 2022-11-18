4d0e2c5ece082c36967e2922e81fc83180bdda02
Elifele Nathan <117702636+ElifeleNathan@users.noreply.github.com>
2022-11-18 11:46:49 +0800

    Project Print in Java

--- Menampilkan Stambuk dan Nama/src/MenampilkanStambukDanNama.java
+++ Menampilkan Stambuk dan Nama/src/MenampilkanStambukDanNama.java
@@ -1,8 +1,4 @@
 public class MenampilkanStambukDanNama {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         System.out.println("2263001");
         System.out.println("I Ketut Natanael Elifele Sarjana");
