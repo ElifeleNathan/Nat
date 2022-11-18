e94b0f9fe18b16b74c121245de22c923f09e8c50
Elifele Nathan <117702636+ElifeleNathan@users.noreply.github.com>
2022-11-18 10:36:52 +0800

    Project Integer in Java

--- Menghitung Hasil Perkalian dari Tiga Bilangan/src/MenghitungHasilPerkalianDariTigaBilangan.java
+++ Menghitung Hasil Perkalian dari Tiga Bilangan/src/MenghitungHasilPerkalianDariTigaBilangan.java
@@ -1,8 +1,10 @@
 public class MenghitungHasilPerkalianDariTigaBilangan {
     public static void main(String[] args) {
+        //inisialisasi dan set
         int nilaia=10;
         int nilaib=20;
         int nilaic=25;
+        //proses perkalian
         int kali=nilaia*nilaib*nilaic;
         System.out.println("hasil kali="+kali);
     }
